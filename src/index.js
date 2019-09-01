fd073d359934e8bf97e60d80819b6126d8f871c1
MalgoskaG <ghouga.m@gmail.com>
2019-09-01 13:52:16 +0200

    deleted unnecessary getMeteoData test

--- src/index.js
+++ src/index.js
@@ -2,12 +2,3 @@ import "./scss/main.scss";
 
 import "./js/townSearcher"
 import "./js/loadingScreen"
-
-import getMeteoData from "./js/meteoData"
-
-const getMeteoDataTest = async () => {
-    const weather = await getMeteoData("Warszawa")
-    console.log(weather)
-}
-
-getMeteoDataTest()
