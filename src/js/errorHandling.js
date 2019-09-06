40414423d86e4807cecc46c3056c0ec6901df636
Gengar-i <rmmswoboda@gmail.com>
2019-09-06 13:22:38 +0200

    errorHandling poprawa

--- src/js/errorHandling.js
+++ src/js/errorHandling.js
@@ -1,16 +1,28 @@
 // sprawdzanie połączenia z internetem
 const main = document.querySelector(".main");
-window.addEventListener("online", function() {
-    const node = main.firstElementChild
-    main.removeChild(node);
-})
-window.addEventListener("offline", function() {
-    const node = document.createElement("div");
-    const textnode = document.createTextNode("brak dostępu do sieci");
-    const child = document.querySelector(".main__box1"); 
-    node.classList.add("no-connection") 
-    node.appendChild(textnode);                        
-    main.insertBefore(node, child); 
-})
-
+const ifoffline = () => {
+    if(!navigator.onLine) {
+        const node = document.createElement("div");
+        const textnode = document.createTextNode("brak dostępu do sieci");
+        const child = document.querySelector(".main__box1"); 
+        node.classList.add("no-connection") 
+        node.appendChild(textnode);                        
+        main.insertBefore(node, child); 
+    }
+}     
+ifoffline(); //wywołuję tą funkcję tylko raz na początku aby sprawdzić czy jest offline aby dodać powiadomienie. W mojej przeglądarce listener nie obsługiwał tego na początku.
+const ifonline = (() => {
+    window.addEventListener("online",() => {
+        const node = main.firstElementChild
+        main.removeChild(node);
+    })
+    window.addEventListener("offline",() => {
+        const node = document.createElement("div");
+        const textnode = document.createTextNode("brak dostępu do sieci");
+        const child = document.querySelector(".main__box1"); 
+        node.classList.add("no-connection") 
+        node.appendChild(textnode);                        
+        main.insertBefore(node, child); 
+    })
+})();   
 //sprawdzanie kolejnego problemu
